31908305183c85dd85050a4392e029f62354aa56
Immi11-8 <imran.backup5286@gmail.com>
2024-08-10 00:56:26 +0500

    sound changes

--- jsmusic.js
+++ jsmusic.js
@@ -1,14 +1,12 @@
-window.ConfettiGenerator = function(e) {
-  // Your existing code here
-
+document.addEventListener('DOMContentLoaded', function() {
   var audio = document.getElementById('background-music');
+  var giftboxButton = document.querySelector('.giftbox');
 
   function playMusic() {
     audio.play();
   }
 
-  // Ensure you select the correct element for "Click Me!" button
-  document.querySelector('.giftbox:after').addEventListener('click', playMusic);
-
-  // Rest of your code here
-};
+  if (giftboxButton) {
+    giftboxButton.addEventListener('click', playMusic);
+  }
+});
